3cfdbce7a504b5ef349437eef02dffd50fd13daf
hyeonic <evan3566@gmail.com>
2020-08-23 23:08:05 +0900

    router & component design

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
+
+Vue.use(VueRouter); // 플러그인 실행/ 초기화 하기 위한 코드
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/LoginPage.vue'), // 코드 스플릿트
+    },
+    {
+      path: '/signup',
+      component: () => import('@/views/SignupPage.vue'),
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
